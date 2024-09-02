8e75b211be55d7100676ff11bf0d5fd81e185603
LeoGrayson <rioferdipurwanto@gmail.com>
2024-09-02 21:41:28 +0700

    login form

---
+++ resources/views/register.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>REGISTRASI</title>
+    <link rel="stylesheet" href="text/css" href="app.css">
+    <link rel="stylesheet" href="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
+</head>
+<body>
+    <div class="container" id="main">
+        <div class="sign-up">
+            <form action="#">
+                <h1>Create Account</h1>
+                <a href="#" class="social"><i class="fa-brands fa-facebook-f"></i></a>
+                <a href="#" class="social"><i class="fa-brands fa-google"></i></a>
+                <a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
+                <p>or use your email for registration</p>
+                <input type="text" name="txt" placeholder="Name" required="">
+                <input type="email" name="email" placeholder="Email" required="">
+                <input type="password" name="pswd" placeholder="Password" required="">
+                <button>Sign Up</button>
+            </form>
+        </div>
+    </div>
+    <div class="container" id="main">
+        <div class="sign-in">
+            <form action="#">
+                <h1>Sign In</h1>
+                <a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
+                <a href="#" class="social"><i class="fab fa-google-g"></i></a>
+                <a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
+                <p>or use your account</p>
+                <input type="text" name="txt" placeholder="Name" required="">
+                <input type="email" name="email" placeholder="Email" required="">
+                <a href="#">Forget your Password?</a>
+                <button>Sign In</button>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
