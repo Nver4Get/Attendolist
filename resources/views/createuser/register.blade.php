ada65db06fe1bbd29b03155c8d45cc1861680aa4
LeoGrayson <rioferdipurwanto@gmail.com>
2024-09-03 20:51:55 +0700

    Updated more
    The other update for register

--- resources/views/createuser/register.blade.php
+++ resources/views/createuser/register.blade.php
@@ -32,12 +32,12 @@
     <div class="overlay-container">
         <div class="overlay">
             <div class="overlay-left">
-                <h1>WELCOME TO <div>ATTENDANCE</div> <div>&</div> <div>TODO LIST</div></h1>
+                <h1>WELCOME TO<br>ATTENDANCE<br>&<br>TODO LIST</h1>
                 <p>If you already have an account, please log in.</p>
                 <button id="signIn">SIGN IN</button>
             </div>
             <div class="overlay-right">
-                <h1>WELCOME TO <br>ATTENDANCE <br>& <br>TODO LIST</h1>
+                <h1>WELCOME TO<br>ATTENDANCE<br>&<br>TODO LIST</h1>
                 <p>This is used to fill in employee attendance <br> and is used to list your daily tasks while working.</p>
                 <button id="signUp">SIGN UP</button>
             </div>
