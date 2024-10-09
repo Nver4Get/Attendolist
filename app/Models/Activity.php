9a0c4cb6ec1fffc602868b71240e21eb349752c5
LeoGrayson <rioferdipurwanto@gmail.com>
2024-10-09 09:59:22 +0700

    up to date fillable
    update fillable

--- app/Models/Activity.php
+++ app/Models/Activity.php
@@ -8,5 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Activity extends Model
 {
     use HasFactory;
-    protected $fillable = ['id_status', 'tasks', 'progress', 'id_user'];
+    protected $fillable = ['attendance', 'tasks', 'progress', 'id_user'];
 }
