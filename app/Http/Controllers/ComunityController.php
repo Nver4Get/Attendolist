5c078fab3e8e54e3746e111a4b00e8e9968c23da
Nver4Get <dante1692006@gmail.com>
2024-10-23 08:51:45 +0700

    update + button

---
+++ app/Http/Controllers/ComunityController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Comunity;
+use App\Http\Requests\StoreComunityRequest;
+use App\Http\Requests\UpdateComunityRequest;
+
+class ComunityController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreComunityRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Comunity $comunity)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Comunity $comunity)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateComunityRequest $request, Comunity $comunity)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Comunity $comunity)
+    {
+        //
+    }
+}
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,13 +5,13 @@
     ?>
     <div class="container div-table">
         <div class="container mt-5 text-center" style="max-width: 750px;">
-            <caption class="h4">Welcome to AttenDoList, please fill your attendance, tasks and don't forget to fill your progress tasks</caption>
+            <caption class="h4">Welcome to AttenDoList, please fill your attendance, tasks and don't forget to fill your
+                progress tasks</caption>
             <div class="dmy mt-3">
                 <caption class="h5 mt-3">D/M/Y</caption>
             </div>
         </div>
-        <div class="table-responsive">
-            <table class="table table-sm caption-top custom-table">
+        <di<div class="table-responsive">
             <table class="table table-sm caption-top custom-table" border="1">
                 <thead>
                     <tr class="">
@@ -23,41 +23,52 @@
                 </thead>
                 <tbody>
                     <tr>
-                        <th scope="row">Sunday</th>
-                        <td>
-                            <a href="#">
-                                <i class="fa-solid fa-plus" <?php if (!$sunday_filled) {
-                                    echo 'style="pointer-events: none; opacity: 0.5;"';
-                                } ?>></i>
-                            </a>
-                        </td>
+                        <th scope="row">Monday</th>
+                        <td><a href="#" class="plus-button" data-day="1"><i class="fa-solid fa-plus"></i></a></td>
                     </tr>
                     <tr>
-                        <th scope="row">Monday</th>
-                        <td><a href="#"><i class="fa-solid fa-plus"></i></a></td>
-                    </tr>
-                    <tr class="tuesday">
                         <th scope="row">Tuesday</th>
-                        <td><a href="#"><i class="fa-solid fa-plus"></i></a></td>
+                        <td><a href="#" class="plus-button" data-day="2"><i class="fa-solid fa-plus"></i></a></td>
                     </tr>
                     <tr>
                         <th scope="row">Wednesday</th>
-                        <td><a href="#"><i class="fa-solid fa-plus"></i></a></td>
+                        <td><a href="#" class="plus-button" data-day="3"><i class="fa-solid fa-plus"></i></a>
                     </tr>
                     <tr>
                         <th scope="row">Thursday</th>
-                        <td><a href="#"><i class="fa-solid fa-plus"></i></a></td>
+                        <td><a href="#" class="plus-button" data-day="4"><i class="fa-solid fa-plus"></i></a></td>
                     </tr>
                     <tr>
                         <th scope="row">Friday</th>
-                        <td><a href="#"><i class="fa-solid fa-plus"></i></a></td>
+                        <td><a href="#" class="plus-button" data-day="5"><i class="fa-solid fa-plus"></i></a></td>
                     </tr>
                     <tr>
                         <th scope="row">Saturday</th>
-                        <td><a href="#"><i class="fa-solid fa-plus"></i></a></td>
+                        <td><a href="#" class="plus-button" data-day="6"><i class="fa-solid fa-plus"></i></a></td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Sunday</th>
+                        <td><a href="#" class="plus-button" data-day="0"><i class="fa-solid fa-plus"></i></a></td>
                     </tr>
                 </tbody>
             </table>
-        </div>
     </div>
+    </div>
+
+    <script>
+        // Ambil semua tombol di kolom Report/Progress
+
+        const reportButtons = document.querySelectorAll('td:last-child a.plus-button');
+        var today = new Date().getDay();
+
+        // Mendapatkan semua tombol plus
+        var plusButtons = document.querySelectorAll('.plus-button');
+
+        // Sembunyikan semua tombol plus kecuali yang sesuai dengan hari ini
+        plusButtons.forEach(function(button) {
+            if (parseInt(button.getAttribute('data-day')) !== today) {
+                button.style.display = 'none';
+            }
+        });
+    </script>
 @endsection
